6cfcdc683b68b30684d3dca88a260c7860130be5
ANOUAR00-1 <b.anouar.officiel@gmail.com>
2026-04-21 14:58:36 +0100

    modifier authController

--- api_auth/src/controllers/authController.js
+++ api_auth/src/controllers/authController.js
@@ -37,7 +37,7 @@ export const login = async (req, res) => {
 
 
 // verifyToken
-const verifyToken = (req, res, next) => {
+export const verifyToken = (req, res, next) => {
  const authHeader = req.headers['authorization'];
  const token = authHeader && authHeader.split(' ')[1];
  if (!token) return res.status(401).send("Unauthorized");
